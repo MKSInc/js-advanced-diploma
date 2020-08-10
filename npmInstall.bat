4d7d8ab4847384b94187cf9b8311b6443bf02ed4
Maksim <pyshkov.maksim@gmail.com>
2020-08-10 13:40:48 +0300

    Добавлено: config файлы

---
+++ npmInstall.bat
@@ -0,0 +1,11 @@
+call npm install --save-dev @babel/core @babel/cli @babel/preset-env
+call npm install core-js@3
+call npm install --save-dev webpack webpack-cli babel-loader
+call npm install --save-dev webpack-dev-server
+call npm install --save-dev html-loader
+call npm install --save-dev html-webpack-plugin
+call npm install --save-dev mini-css-extract-plugin css-loader
+call npm install --save-dev url-loader
+call npm install --save-dev jest babel-jest
+call npm install --save-dev eslint
+call npx eslint --init
